de2f3be40929d29d9654522ba32fbeed122cf220
isabelle-galleberg <isabelle-galleberg@users.noreply.github.com>
2026-05-03 10:31:28 +0200

    Remove custom errors

--- main.tf
+++ main.tf
@@ -150,18 +150,6 @@ resource "aws_cloudfront_distribution" "preview" {
     }
   }
 
-  custom_error_response {
-    error_code         = 404
-    response_code      = 200
-    response_page_path = "/index.html"
-  }
-
-  custom_error_response {
-    error_code         = 403
-    response_code      = 200
-    response_page_path = "/index.html"
-  }
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
